c47edcedd0d05f3b73e5a9894758c0ca33ab1b73
hello30190f <w5w5ww6w6w@gmail.com>
2023-02-12 18:07:24 +0900

    I add a tip by adding url

--- test/2/videoNext.py
+++ test/2/videoNext.py
@@ -4,3 +4,5 @@ import vlc
 vlcIns = vlc.MediaPlayer(filedialog.askopenfilename())
 while True:
     vlcIns.play()
+
+# https://stackoverflow.com/questions/47990695/how-to-embed-a-vlc-instance-in-a-tkinter-frame
