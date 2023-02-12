9461d0f798c5c2f1e16a574c77071a5a5942533b
hello30190f <w5w5ww6w6w@gmail.com>
2023-02-12 18:33:18 +0900

    add tkinter sample built-in vlc video player

--- test/2/videoNext.py
+++ test/2/videoNext.py
@@ -1,8 +1,46 @@
 from tkinter import filedialog
 import vlc
+import customtkinter as tk
 
-vlcIns = vlc.MediaPlayer(filedialog.askopenfilename())
-while True:
-    vlcIns.play()
+def videoPlayWay():
+    vlcIns = vlc.MediaPlayer(filedialog.askopenfilename())
+    while True:
+        vlcIns.play()
 
 # https://stackoverflow.com/questions/47990695/how-to-embed-a-vlc-instance-in-a-tkinter-frame
+
+import ctypes
+try:
+    ctypes.windll.shcore.SetProcessDpiAwareness(True)
+except:
+    pass
+
+# def video(display:tk.CTkFrame):
+
+    # pass
+
+# import tkinter as tk
+res = "800x700"
+window_name = "sample video player"
+root = tk.CTk()
+root.geometry(res)
+root.title(window_name)
+
+Main = tk.CTkFrame(root,width=700,height=700)
+Main.grid(row=0,column=0)
+
+displayP = tk.CTkFrame(Main)
+displayP.place(relwidth=1,relheight=1)
+
+
+Instance = vlc.Instance()
+player = Instance.media_player_new()
+Media = Instance.media_new(filedialog.askopenfilename())
+player.set_hwnd(displayP.winfo_id())
+player.set_media(Media)
+player.play()
+# button = tk.CTkButton(root,text="Try play video")
+# button.bind("<1>",lambda self:video(display=displayP))
+# button.grid(row=0,column=1)
+
+root.mainloop()
