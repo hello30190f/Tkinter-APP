dd51eb7a2cc317be646cebb1f2ddfd097eafffa6
hello30190f <w5w5ww6w6w@gmail.com>
2023-03-03 14:54:47 +0900

    I add function to data.py

--- works/2/video/data.py
+++ works/2/video/data.py
@@ -1,3 +1,5 @@
+import ffmpeg
+
 class Data:
     _ImgArray = []
     _width = 0
@@ -6,37 +8,60 @@ class Data:
     _FrameAmount:int = 0
     _Lenth:int = 0          # sec
     _audioArray = []
+    _audioFrequency = 0
+
+    def IsOk(self) -> bool:
+        flag:bool = False
+        if(not (self._width and self._height)):
+            flag = False
+        if(not (self._fps and self._FrameAmount)):
+            flag = False
+        if(not (self._audioArray and self._audioFrequency)):
+            flag = False
+        if(not (self._Lenth and self._ImgArray)):
+            flag = False
+        return flag
 
 
-class setData:
-    def setVideoFrame(vidoeImg):
-        pass
-
-    def setVidoefps(fps:int):
-        pass
-
-    def setVidoeSize(width:int,height:int):
-        pass
-    
-    def setFrameAmount(FrameAmount:int):
-        pass
-
-    def setAudioArray(audio):
-        pass
+# class setData:
+    def setVideoFrame(self,vidoeImg):
+        self._ImgArray.append(vidoeImg)
 
 
-class getData:
-    def getLenth() -> int: # sec
-        pass
+    def setVidoefps(self,fps:int):
+        self._fps = fps
 
-    def getVideoFrame(index:int):
-        pass
 
-    def getFps() -> int:
-        pass
+    def setVidoeSize(self,width:int,height:int):
+        self._width = width
+        self._height = height
 
-    
 
-    pass
+    def setFrameAmount(self,FrameAmount:int):
+        self._FrameAmount = FrameAmount
 
-     
+
+    def setAudioArray(self,audio):
+        self._audioArray.append(audio)
+
+
+    def setAudioFrequency(self,Hz:int):
+        self._audioFrequency = Hz
+
+# class getData:
+    def getLenth(self) -> int: # sec
+        return self._Lenth
+
+
+    def getVideoFrame(self,index:int):
+        return self._ImgArray[index]
+
+
+    def getFps(self) -> int:
+        return self._fps
+
+
+
+
+
+
