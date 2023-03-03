b334c820d1722996b3a7ff160695e27129f8744b
hello30190f <w5w5ww6w6w@gmail.com>
2023-03-03 14:20:10 +0900

    I added code to data.py

---
+++ works/2/video/data.py
@@ -0,0 +1,42 @@
+class Data:
+    _ImgArray = []
+    _width = 0
+    _height = 0
+    _fps = 0
+    _FrameAmount:int = 0
+    _Lenth:int = 0          # sec
+    _audioArray = []
+
+
+class setData:
+    def setVideoFrame(vidoeImg):
+        pass
+
+    def setVidoefps(fps:int):
+        pass
+
+    def setVidoeSize(width:int,height:int):
+        pass
+    
+    def setFrameAmount(FrameAmount:int):
+        pass
+
+    def setAudioArray(audio):
+        pass
+
+
+class getData:
+    def getLenth() -> int: # sec
+        pass
+
+    def getVideoFrame(index:int):
+        pass
+
+    def getFps() -> int:
+        pass
+
+    
+
+    pass
+
+     
