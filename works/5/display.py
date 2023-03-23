2068b8bdf0c30d22afc6af668fba4309b2bbd352
hello30190f <108202763+hello30190f@users.noreply.github.com>
2023-03-24 00:21:02 +0900

    Making physics part

--- works/5/display.py
+++ works/5/display.py
@@ -16,6 +16,9 @@ class lenth:
         self.dpi = 246
         self.OneInchToCM = 2.54 # cm
 
+        self.CanvasWidth = 600
+        self.CanvasHeight = 50
+
     def clacDpi(self):
         tempWidth = self.ScreenWidth ^ 2
         tempHeight = self.ScreenHeight ^ 2
@@ -29,27 +32,32 @@ class lenth:
         return self.OneInchToCM * Inch
 
 
-    def barControl(self,canvas:tk.CTkCanvas,id):
-        
-        pass
+    def barControl(self,canvas:tk.CTkCanvas,event):
+        # position = canvas.bbox(self.id)
+        if(event.x >= 0 and event.x <= self.CanvasWidth - self.size):
+            canvas.delete(self.id)
+            self.id = canvas.create_rectangle(event.x,10,event.x+self.size,10+self.size,fill="Red")
+            position = canvas.bbox(self.id)
+            self.barPosition = position[0] - 10
+            print(self.barPosition)
 
 
-    def sliderByCanvas(self,frame:tk.CTkFrame):
-        canvas = tk.CTkCanvas(frame,width=frame.winfo_width(),height=frame.winfo_height(),bg="gray")
+    def sliderByCanvas(self,frame:tk.CTkFrame,root:tk.CTk):
+        self.size = self.CanvasHeight - 20
+        canvas = tk.CTkCanvas(frame,width=self.CanvasWidth,height=self.CanvasHeight,bg="gray")
         canvas.pack(padx=10,pady=10)
-        canvas.create_rectangle(10,10,frame.winfo_width-10,frame.winfo_height-10,fill="Green")
-        self.size = frame.winfo_height - 20
-        id = canvas.create_rectangle(10,10,self.size+10,self.size+10)
-        canvas.bind("<B1-Motion>",lambda args:self.barControl(canvas,id))
+        canvas.create_rectangle(10,10,self.CanvasWidth-10,self.CanvasHeight-10,fill="Green")
+        self.id = canvas.create_rectangle(10,10,10+self.size,10+self.size,fill="Red")
+        self.barPosition = 0  # 10 + self.size / 2
+        self.barPositionOffset = 10
+        canvas.bind("<B1-Motion>",lambda args:self.barControl(canvas,args))
         
 
 
     def lenthAdapter(self,root:tk.CTk):
-        frame = tk.CTkFrame(root)
-
-
+        frame = tk.CTkFrame(root,width=self.CanvasWidth,height=self.CanvasHeight)
+        self.sliderByCanvas(frame,root)
         frame.pack(pady=10,padx=10)
-        pass
 
 
 
@@ -59,8 +67,10 @@ if __name__ == "__main__":
     res = "500x500"
     window_name = "ex"
     root = tk.CTk()
+    ins = lenth(root)
     root.geometry(res)
     root.title(window_name)
 
+    ins.lenthAdapter(root)
 
     root.mainloop()
