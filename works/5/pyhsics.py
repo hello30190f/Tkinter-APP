2aa0b719691481ed1dd2376fc99d5fb301313267
hello30190f <108202763+hello30190f@users.noreply.github.com>
2023-03-23 22:27:56 +0900

    Making physics part

--- works/5/pyhsics.py
+++ works/5/pyhsics.py
@@ -2,7 +2,7 @@ import math
 import customtkinter as tk
 
 class Vector2:
-    
+
     def __init__(self,x = 0,y = 0) -> None:
         self.x = x
         self.y = y
@@ -19,7 +19,7 @@ class obj:
         self.canvasId = canvasId
         self.canvas = canvas
 
-    def getMiddlePosition(self):
+    def getMiddlePosition(self) -> Vector2:
         positionEdges = self.canvas.bbox(self.canvasId)
         width = positionEdges[2] - positionEdges[0]
         height = positionEdges[3] - positionEdges[1]
@@ -31,14 +31,60 @@ class obj:
     def UpdateCanvasId(self,CanvasId):
         self.canvas = CanvasId
 
-def VectorSum(a:Vector2,b:Vector2):
+class spring:
+
+    def __init__(self,lenth = 10,constant = 1) -> None:
+        self.lenth = lenth
+        self.constant = constant
+
+# -----------------------------------------------------------------------
+
+def VectorSum(a:Vector2,b:Vector2) -> Vector2:
     x = a.x + b.x
     y = a.y + b.y
     temp = Vector2(x,y)
     return temp
 
-def clacForce(canvas:tk.CTkCanvas):
-    
+def VectorSub(a:Vector2,b:Vector2) -> Vector2:
+    temp = Vector2(-b.x,-b.y)
+    return VectorSum(a,temp)
+
+def VectorLenth(a:Vector2) -> float:
+    lenth = math.sqrt(a.x^2 + a.y^2)
+    return lenth
+
+
+# -----------------------------------------------------------------------
+
+class physicsClac:
+
+    def positionDiff(a:Vector2,b:Vector2):
+        temp = Vector2((a.x - b.x),(a.y - b.y))
+
+
+    def clacSpringForce(self,canvas:tk.CTkCanvas,Pa:obj,Pb:obj,springC:spring,Interval = 16):
+        ForceAx = springC.constant * (Pb.position.x - Pa.position.x)
+        ForceAy = springC.constant * (Pb.position.y - Pa.position.y) # y axis inverted
+
+        ForceBx = springC.constant * (Pa.position.x - Pb.position.x)
+        ForceBy = springC.constant * (Pa.position.y - Pb.position.y)
+
+        A = Vector2(ForceAx,ForceAy)
+        B = Vector2(ForceBx,ForceBy)
+
+        return A,B
+
+
+
+
+
+
+# -----------------------------------------------------------------------
+
+
+
+def clacForce(canvas:tk.CTkCanvas,Pa:obj,Pb:obj,springC:spring,Interval = 16):
+    ForceA = -springC.constant
     pass
 
 
