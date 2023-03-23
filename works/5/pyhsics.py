35359ee9aab50c4a8c66f58beed56c0e990d0ff5
hello30190f <108202763+hello30190f@users.noreply.github.com>
2023-03-23 20:46:51 +0900

    Making physics part

---
+++ works/5/pyhsics.py
@@ -0,0 +1,24 @@
+import math
+import customtkinter as tk
+
+class Vector2:
+    
+    def __init__(self,x = 0,y = 0) -> None:
+        self.x = x
+        self.y = y
+
+    def setElem(self,x,y):
+        self.x = x
+        self.y = y
+
+def VectorSum(a:Vector2,b:Vector2):
+    x = a.x + b.x
+    y = a.y + b.y
+    temp = Vector2(x,y)
+    return temp
+
+
+# def multiplication(a:Vector2,b:Vector2):
+#     x = a.x * b.x
+#     y = a.y * b.y
+#     result = Vector2()
