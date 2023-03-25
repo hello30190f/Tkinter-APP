5a6339a1dc6ddd53196d94fe6efbe2ed8eaaa3cb
hello30190f <w5w5ww6w6w@gmail.com>
2023-03-26 00:01:19 +0900

    Testing feature of json moudle.

--- works/5/test/jsontest.py
+++ works/5/test/jsontest.py
@@ -2,8 +2,44 @@ import json
 from tkinter import filedialog
 
 def readJson():
+    result = None
+    result:dict
     with filedialog.askopenfile("r") as file:
-        reuslt = json.load(file)
-        print(reuslt)
+        result = json.load(file)
+        # print(result)
+
+    # print(result["A things"])
+    # print(result.keys().__str__())
+    # temp = result[result.keys().__str__()]
+
+    # for test in result:
+    #     # print(test)
+    #     for elem in result[test]:
+    #         print(elem)
+
+        readDict(result)
+    
+
+def readDict(data:dict):
+    resultArray = []
+    temp = []
+    def readDictR(data:dict):
+        try:
+            for elem in data:
+                resultArray.append(elem)
+                temp.append(data.values())
+                readDictR(data[elem])
+        except:
+            pass
+    readDictR(data)
+    print(resultArray)
+    # print(temp)
+    for text in temp:
+        print(text)
+        print()
+
+
+
+
 
 readJson()
